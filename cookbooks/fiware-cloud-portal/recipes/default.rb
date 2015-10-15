99b4d9c24cc827db2c5e9891b59b920067616ce2
naruhodo-ryuichi <pm.verdugo@alumnos.upm.es>
2015-10-15 16:44:38 +0200

    node cache permissions fix

--- cookbooks/fiware-cloud-portal/recipes/default.rb
+++ cookbooks/fiware-cloud-portal/recipes/default.rb
@@ -9,9 +9,9 @@ end
 
 bash :get_system do
   code <<-EOH
-    sudo cd /opt && \
+    cd /opt && \
     sudo git clone https://github.com/ging/fiware-cloud-portal.git && \
-    sudo cd #{node['fiware-cloud-portal'][:app_dir]} && \
+    cd #{node['fiware-cloud-portal'][:app_dir]} && \
     sudo mkdir $HOME/.npm && \
     sudo chown -R $(whoami) "$HOME/.npm" && \
     sudo npm install && \
